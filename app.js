862efc3618f8bf6557ec9591702a237f8f63ffad
Viktor Dimalanta <dimalantaviktorangelo@gmail.com>
2023-12-07 11:03:18 +0800

    update package json and template views

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const request = require('request');
+const cors = require('cors');
 const dotenv = require('dotenv');
 const app = express();
 const port = 3000;
 
-//app.use(cors());
-// Load environment variables from a .env file
 dotenv.config();
+
+// Update the cors middleware based on your requirements
+app.use(cors());
 
 const mapboxApiKey = process.env.MAPBOX_API_KEY;
 const openWeatherMapApiKey = process.env.OPENWEATHERMAP_API_KEY;
@@ -19,36 +21,26 @@ app.get('/', (req, res) => {
 });
 
 app.get('/weather', (req, res) => {
-  const location = req.query.location;
+  const coordinates = req.query.coordinates.split(',').map(Number);
+  const location = `(${coordinates.join(', ')})`;
 
-  // Use Mapbox API to get coordinates for the location
-  const mapboxUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(location)}.json?access_token=${mapboxApiKey}`;
+  // Use the coordinates to get weather data from OpenWeatherMap
+  const openWeatherMapUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${coordinates[1]}&lon=${coordinates[0]}&appid=${openWeatherMapApiKey}`;
 
-  request({ url: mapboxUrl, json: true }, (error, response, body) => {
+  request({ url: openWeatherMapUrl, json: true }, (error, response, weatherData) => {
     if (error || response.statusCode !== 200) {
-      return res.status(500).json({ error: 'Unable to fetch location data' });
+      return res.status(500).json({ error: 'Unable to fetch weather data' });
     }
 
-    const coordinates = body.features[0].center;
+    // Extract relevant weather information
+    const temperature = weatherData.main.temp;
+    const weatherDescription = weatherData.weather[0].description;
 
-    // Use the coordinates to get weather data from OpenWeatherMap
-    const openWeatherMapUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${coordinates[1]}&lon=${coordinates[0]}&appid=${openWeatherMapApiKey}`;
-
-    request({ url: openWeatherMapUrl, json: true }, (error, response, weatherData) => {
-      if (error || response.statusCode !== 200) {
-        return res.status(500).json({ error: 'Unable to fetch weather data' });
-      }
-
-      // Extract relevant weather information
-      const temperature = weatherData.main.temp;
-      const weatherDescription = weatherData.weather[0].description;
-
-      res.render('weather', {
-        location,
-        coordinates,
-        temperature,
-        weatherDescription,
-      });
+    res.render('weather', {
+      location,
+      coordinates,
+      temperature,
+      weatherDescription,
     });
   });
 });
