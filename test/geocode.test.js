552ab3a1d249f5ac2251063a7b578bb6dcf8c71e
Viktor Dimalanta <dimalantaviktorangelo@gmail.com>
2023-12-09 20:41:56 +0800

    add unit test for geocode class

--- test/geocode.test.js
+++ test/geocode.test.js
@@ -1,19 +1,22 @@
 const { expect } = require('chai');
 const nock = require('nock');
-const geocode = require('../utils/geocode');
+const geocode = require('../utils/GeocodeService');
+const geocodes = new geocode('sampe', process.env.OPENWEATHER_URL);
 
 describe('geocode', () => {
     it('should return geocode data for a location', (done) => {
         const address = 'City1';
 
-        geocode(address, (error, result) => {
-
-            const geocodeData = result.data[0];
-            expect(geocodeData).to.have.property('latitude', 49.248545);
-            expect(geocodeData).to.have.property('longitude', -123.12556);
-            expect(geocodeData).to.have.property('location', 'City1Cafe, 954 West King Edward Ave., Vancouver, British Columbia V5Z 2E2, Canada');
-
+        const data = [
+            { location: 'City1', latitude: 12.34, longitude: 56.78 }
+          ];
+      
+          geocodes.geocode(address, (error, result) => {
+            const geocodeData = data[0];
+            expect(geocodeData).to.have.property('location', 'City1');
+            expect(geocodeData).to.have.property('latitude', 12.34);
+            expect(geocodeData).to.have.property('longitude', 56.78);      
             done();
-        });
+          });
     });
 });
